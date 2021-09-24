6b389468cdde49fc2b2a91f2310439ec58cd9dfa
Pawar <Nilesh.Pawar@saint-gobain.com>
2021-09-24 22:47:20 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.o-grid--left').hover(function () {
+        $('.o-grid--3').removeClass('active');
+        $(this).parent('.o-grid--3').addClass('active');
+        $(this).closest('.o-grid').addClass('open');
+        $('.o-grid--right').hide();
+        $(this).next('.o-grid--right').slide();
+    });
+
+    $('.menu-icon').click(function () {
+        $('nav').slideToggle();
+    })
+
+    $('nav ul li').each(function(index) {
+        const count = index;
+        $(this).click(function() {
+            console.log(count);
+            if($(window).width() <= 960) {
+                $('nav').slideUp('fast');
+            }
+            var element = document.querySelector('#section-'+ count)
+            element.scrollIntoView({behavior: "smooth"});
+        })
+    })
+
+})
